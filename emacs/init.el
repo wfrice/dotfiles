eaa77107add910b0d421fae74a261afba72dcf97
wfrice <ricewilliam125@gmail.com>
2022-05-20 09:40:53 -0400

    Changed emacs modeline

--- emacs/init.el
+++ emacs/init.el
@@ -3,9 +3,11 @@
 
 (menu-bar-mode -1)	; Disables the menu bar
 
-(load-theme 'tango-dark)
+(load-theme 'wombat)
 
 
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
 
 ;; Initialize package sources
@@ -19,6 +21,7 @@
 (unless package-archive-contents
   (package-refresh-contents))
 
+
 ;; Initialize use-package on non-Linux platform
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
@@ -26,7 +29,10 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-(use-package command-log-mode)   ; Keeps track of commands and explains said command
+(use-package command-log-mode)  ;; Keeps track of commands and explains said command
+(use-package diminish)          ;; Allows the hiding of minor-modes
+(use-package all-the-icons      ;; Brings in icons
+  :ensure t)
 
 ;; Use Ivy for completions
 (use-package ivy
@@ -46,3 +52,19 @@
 	 ("c-d" . ivy-reverse-i-search-kill))
   :config
   (ivy-mode 1))
+
+
+;; Use Counsel for file switching
+(use-package counsel 
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map
+	 ("C-r" . counsel-minibuffer-history))
+  :config
+  (setq ivy-initial-inputs-alist nil)) ;; Dont begin searches with ^
+
+;; Use doom-modeline for mode line
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
