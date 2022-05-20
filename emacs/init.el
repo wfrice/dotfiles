4aa47a02ef647d2a4b96aa8e45ca4a7e8da097e8
wfrice <ricewilliam125@gmail.com>
2022-05-20 06:50:31 -0400

    emacs changes

---
+++ emacs/init.el
@@ -0,0 +1,48 @@
+
+(setq inhibit-startup-message t)
+
+(menu-bar-mode -1)	; Disables the menu bar
+
+(load-theme 'tango-dark)
+
+
+
+
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platform
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package command-log-mode)   ; Keeps track of commands and explains said command
+
+;; Use Ivy for completions
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+	 :map ivy-minibuffer-map
+	 ("TAB" . ivy-alt-done)
+	 ("C-l" . ivy-alt-done)
+	 ("C-j" . ivy-next-line)
+	 ("C-k" . ivy-previous-line)
+	 :map ivy-switch-buffer-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-l" . ivy-done)
+	 ("C-d" . ivy-switch-buffer-kill)
+	 :map ivy-reverse-i-search-map
+	 ("C-k" . ivy-previous-line)
+	 ("c-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
